8413dcdb0f812bee6ef0bc24fbc5506f26d1a94d
SantinoSiciliano <santinosiciliano05@gmail.com>
2023-08-26 20:40:08 -0300

    0.03:consecsionaria

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const totalDeAutos = parseInt(prompt('total de autos a la venta esta semana' ));
-        let contDeAutos = 0;
-
-        for (let cant= 0; cant < totalDeAutos; cant++){
-        const resp= prompt(' auto numero ' +  cant  + ' fue vendido ?');
-        if (resp === 'si') {
-               contDeAutos++; 
-            }
-        }
-        const contadorDeAutosVendidos = contDeAutos * 100/ totalDeAutos;
-        alert(contadorDeAutosVendidos + '% de autos fueron vendidos esta semana')
+class Auto {
+    constructor(nombreDelAuto,precioDelAuto) {
+      this.nombreDelAuto = nombreDelAuto
+      this.precioDelAuto = precioDelAuto
+    }
+  }
+  
+//la compra del auto
+  function procesoDeCompraDelAuto() {
+    const precioDelAuto = parseFloat(prompt('ingrese el precio del vehiculo'))
+    const nombreDelAuto = prompt('ingrese el nombre del vehiculo que compro')
+    return new Auto(nombreDelAuto,precioDelAuto)
+  }
+//pregunta de seguir comprando  
+  const Autos = []
+     
+  let continuaComprandoAutos = true
+  while (continuaComprandoAutos) {
+    const aut = procesoDeCompraDelAuto()
+  
+    Autos.push(aut)
+  
+    const respuesta = prompt('quiere comprar algun autos mas? si/no')
+    if (respuesta === 'no') {
+        continuaComprandoAutos = false
+    }
+  }
+// total del auto  
+  let total = 0
+  const precio = Autos.map(p => p.precioDelAuto)
+  precio.forEach(p => { total = total + p })
+  
+  alert('precio total de vehiculo adquirido'   +   total)
