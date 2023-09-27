16cde6acb113e320f31e73b4fa0a9ce9e09b5f2b
SantinoSiciliano <santinosiciliano05@gmail.com>
2023-09-27 16:01:11 -0300

    0.05

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const formCompra = document.querySelector('#formCompra');
 const inputNombre = document.querySelector('#inputNombre');
 const inputPrecio = document.querySelector('#inputPrecio');
-const divListaDeVehiculos = document.querySelector('#listaDeVehiculos');
+const ListaDeVehiculos = document.querySelector('#listaDeVehiculos');
 const seccionCompraRealizada = document.querySelector('#seccionCompraRealizada');
 
 // Obtener datos de autos desde el almacenamiento local
@@ -18,25 +18,28 @@ class Vehiculo {
 // Manejar el envío del formulario
 formCompra.onsubmit = e => {
   // Evitar que se recargue la página por defecto
-  e.preventDefault(); 
-// Obtener el nombre y el precio ingresados en el formulario
+  e.preventDefault();
+  // Obtener el nombre y el precio ingresados en el formulario
   const nombre = inputNombre.value;
   const precio = inputPrecio.value;
 
   const auto = new Vehiculo({ nombre, precio });
-// guardar el auto en la lista 
+  // guardar el auto en la lista 
   guardarAuto(auto);
+  // Mostrar la alerta de compra exitosa
+  mostrarCompraExitosa();
 }
 
 function guardarAuto(auto) {
-  autos.push(auto);// Agregar el automóvil a la lista
+  autos.push(auto); // Agregar el automóvil a la lista
   localStorage.setItem('autos', JSON.stringify(autos));
   mostrarCompraRealizada();
 }
+
 // mostrar la compra realizada 
 function mostrarCompraRealizada() {
   if (autos.length > 0) {
-// compra realizada
+    // compra realizada
     seccionCompraRealizada.style.display = 'block';
     let listaVehiculoHtml = '<ul>';
     // construir la lista HTML
@@ -44,6 +47,42 @@ function mostrarCompraRealizada() {
       listaVehiculoHtml += `<li>${auto.nombre}</a></li>`;
     }
     // Mostrar la lista en el HTML
-    divListaDeVehiculos.innerHTML = listaVehiculoHtml + '</ul>';
+    ListaDeVehiculos.innerHTML = listaVehiculoHtml + '</ul>';
   }
 }
+
+// Función para mostrar la alerta de compra exitosa
+function mostrarCompraExitosa() {
+  Swal.fire({
+    title: 'Oferta Exitosa',
+    text: '¡Estamos de acuerdo con tu oferta!',
+    icon: 'success',
+    confirmButtonText: 'Entendido'
+  });
+}
+
+// Función para obtener la hora actual en Buenos Aires
+function obtenerHoraBuenosAires() {
+  // Solicita a la API de World Time
+  fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires')
+    .then(response => response.json())
+    .then(data => {
+      //Respuesta de la API
+      const horaActual = data.datetime;
+
+      // Actualiza el contenido en la página con la hora
+      const horaElement = document.querySelector('#horaActual');
+      horaElement.textContent = `Hora actual en Buenos Aires: ${horaActual}`;
+    })
+    .catch(error => {
+      console.error('Error al obtener la hora:', error);
+    });
+}
+
+// obtiene la hora en Buenos Aires
+obtenerHoraBuenosAires();
+
+
+
+
+
